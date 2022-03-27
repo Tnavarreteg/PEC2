918d903bf90311d08d257dc19e6754047102d30d
tnavarreteg <tnavarreteg@uoc.edu>
2022-03-27 12:54:08 +0200

    ejer1-d

---
+++ PEC2_Ej1/ejer1-d.js
@@ -0,0 +1,61 @@
+function findOne(list, { key, value }){                                                     // función que recibe array de objetos y un objeto
+    
+  return new Promise((onSuccess,onError)  => {                                               // retorna la creación de una promesa la cual devolverá el valor de onSuccess si el valor se encuentra, de lo contrario devolverá onError
+   const element = list.find(element => element[key] === value);                             // se almacena en la constante element el valor que se busca en el array
+  
+
+   setTimeout(() => {                                                                        // funcion recibe el valor element y lapso de tiempo para ser ejecutada 
+    element ? onSuccess('user: ' + element[key]) : onError('ERROR: Element Not Found');      // si la promesa se cumple devolverá el valor de onSuccess de no ser así devolverá el valor de onError 
+   }, 2000);
+
+})};
+
+const users = [                                                                                  // array con objetos
+   {
+     name: 'Carlos',
+     rol: 'Teacher'
+   },
+   {
+     name: 'Ana',
+
+     rol: 'Boss'
+   }
+ ];
+
+
+console.log('findOne success');                                                // imprime en consola
+ async function msg1(){                                                        //
+   try{
+    const msg1 = await findOne(users, { key: 'name', value: 'Carlos'});
+    console.log(msg1);                                                           // imprime en consola la constante msg1 que almacena el resultado de la busqueda 
+   } catch(error){
+     console.log(error);                                                         // imprime en consola
+   }  
+ }
+msg1(); 
+
+console.log('findOne error');                                                    // imprime en consola
+async function msg2(){
+  try{
+   const msg2 = await findOne(users, { key: 'name', value: 'Jean'});
+   console.log(msg2);                                                                // imprime en consola
+  } catch(error){
+    console.log(error);                                                            // imprime en consola
+  }  
+}
+msg2();
+
+
+
+/*
+ findOne success
+ findOne error
+  //wait 2 seconds
+ user: Carlos
+ ERROR: Element Not Found
+ */
+ 
+ /*modifica el código anterior para eliminar los callbacks y, en su lugar, hacer uso de 
+ promesas (hay que crearlas en la función findOne) y consumirlas en el código 
+ principal (con las palabras reservadas then y catch). Documenta en el código, línea 
+ a línea, que cambios has realizado.*/
